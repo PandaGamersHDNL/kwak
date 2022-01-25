b16120c2c0e6e7116ca94177bfa48053d0e514d8
panda jacobs <jokke.jacobs@gmail.com>
2022-01-25 14:29:40 +0100

    initial commit unreal generated files + ignore

---
+++ kwak/Source/kwak/kwak.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class kwak : ModuleRules
+{
+	public kwak(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
